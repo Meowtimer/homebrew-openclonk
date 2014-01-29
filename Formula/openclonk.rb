89e00d2c32b4111015d5ad485d53f10c5e8de80c
Martin Plicht <mardmol@gmx.de>
2014-01-29 17:27:32 +0100

    Remove comments, fix head/url

--- Formula/openclonk.rb
+++ Formula/openclonk.rb
@@ -1,12 +1,9 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Openclonk < Formula
-  head 'https://github.com/openclonk/openclonk/archive/master.zip', :using => :git
-  version "4.0"
+  head 'http://git.openclonk.org/openclonk.git', :using => :git
+  url 'https://github.com/openclonk/openclonk/archive/master.zip'
+  version "4.0+"
 
   depends_on "cmake" => :build
   depends_on "jpeg"
@@ -24,14 +21,6 @@ class Openclonk < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test openclonk`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "true"
   end
 end
